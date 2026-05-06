ea5eb2a115965719b12bace7f5f02a146968bd46
Denis Gosset <denis.gosset@hoteia.com>
2026-05-06 22:49:29 +0200

    Updates

--- _nuxt/COA1SUs8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,J as s}from"./COy4I9nT.js";const a={};function c(e,n){return r(),t("table",null,[s(e.$slots,"default")])}const _=o(a,[["render",c]]);export{_ as default};
